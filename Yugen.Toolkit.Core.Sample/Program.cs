a6bda5f9e517f5d51413b749de41908b9db448f1
Emiliano Magliocca <emiliano.magliocca@outlook.com>
2020-04-09 19:11:07 +0300

    Add EF Core Sample

---
+++ Yugen.Toolkit.Core.Sample/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Yugen.Toolkit.Standard.Data.Extensions;
+using Yugen.Toolkit.Standard.Data.Sample;
+
+/// <summary>
+/// Add a reference to Yugen.Toolkit.Standard.Data.Sample
+///
+/// How To Create a Migration
+/// Select: Startup Project: Yugen.Toolkit.Core.Sample
+/// Go To: Package Manager Console
+/// Select: Default Project: Yugen.Toolkit.Standard.Data.Sample
+/// (Optional) Write: Remove-Migration
+/// Write: Add-Migration {MigrationName}
+/// </summary>
+namespace Yugen.Toolkit.Core.Sample
+{
+    class Program
+    {
+        // this connection string must be retrieved from a secure file.
+        private static readonly string _connectionString = "Data Source=MyDatabase.db";
+
+        private static void ConfigureServices(IServiceCollection isc)
+        {
+            isc.AddDbContext<BloggingContext>(options => options.UseSqlite(_connectionString));
+            //isc.AddSingleton<TheApp>();
+        }
+
+        private static IServiceProvider CreateServiceProvider()
+        {
+            // create service collection
+            IServiceCollection isc = new ServiceCollection();
+            ConfigureServices(isc);
+
+            // create service provider
+            return isc.BuildServiceProvider();
+        }
+
+        private static void Main(string[] args)
+        {
+            using (var scope = CreateServiceProvider().CreateScope())
+            {
+                //scope.ServiceProvider.GetService<TheApp>().Run();
+            }
+        }
+
+        private class Factory : IDesignTimeDbContextFactory<BloggingContext>
+        {
+            public BloggingContext CreateDbContext(string[] args)
+                => CreateServiceProvider().CreateScope().ServiceProvider.GetService<BloggingContext>();
+        }
+    }
+}
---
+++ Yugen.Toolkit.Standard.Data.Sample/BloggingContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+/// <summary>
+/// Add a reference to Yuogen.Toolkit.Standard.Data
+/// </summary>
+namespace Yugen.Toolkit.Standard.Data.Sample
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+
+        public BloggingContext() { }
+        public BloggingContext(DbContextOptions<BloggingContext> options) : base(options) { }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlite("Data Source=blog.db");
+        //}
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+
+        public List<Post> Posts { get; } = new List<Post>();
+    }
+}
