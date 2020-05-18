62818287116a3e4a007f4a96b09856b1a1e4bf3a
Emiliano Magliocca <emiliano.magliocca@outlook.com>
2020-05-18 11:05:21 +0300

    Add new classes and Update nugets

---
+++ Yugen.Toolkit.Standard/Helpers/MathHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Yugen.Toolkit.Standard.Helpers
+{
+    public static class MathHelper
+    {
+        /// <summary>
+        /// Convert from one range to another
+        /// oldMin : oldValue : oldMax = newMin : newValue : newMax
+        /// </summary>
+        /// <param name="oldValue"></param>
+        /// <param name="oldMin"></param>
+        /// <param name="oldMax"></param>
+        /// <param name="newMin"></param>
+        /// <param name="newMax"></param>
+        /// <returns>newValue</returns>
+        public static double RangeConvert(double oldValue, double oldMin, double oldMax, double newMin, double newMax) =>
+            (oldValue - oldMin) * (newMax - newMin) /
+                (oldMax - oldMin) + newMin;
+
+        public static Tuple<int, int> RatioConvert(int width, int height, int newHeight, int newWidth)
+        {
+            //calculate the ratio
+            var ratio = width / (double)height;
+
+            //set height of image to boxHeight and check if resulting width is less than boxWidth, 
+            //else set width of image to boxWidth and calculate new height
+            return (int)(newHeight * ratio) <= newWidth
+                ? new Tuple<int, int>((int)(newHeight * ratio), newHeight)
+                : new Tuple<int, int>(newWidth, (int)(newWidth / ratio));
+        }
+    }
+}
---
+++ Yugen.Toolkit.Uwp/Helpers/ResourceHelper.cs
@@ -0,0 +1,12 @@
+﻿using Windows.ApplicationModel.Resources;
+
+namespace Yugen.Toolkit.Uwp.Helpers
+{
+    public static class ResourceHelper
+    {
+        private static readonly ResourceLoader _resourceLoader = _resourceLoader ?? new ResourceLoader();
+
+        public static string GetText(string key) =>
+            _resourceLoader.GetString(key);
+    }
+}
