57c00e7515a5fc9e0cd4f5b1337b4891a1024ae9
Emiliano Magliocca <emiliano.magliocca@outlook.com>
2020-08-12 12:04:06 +0300

    Add behavior and viewmodel

--- Yugen.Toolkit.Uwp.Samples/AppShell.xaml.cs
+++ Yugen.Toolkit.Uwp.Samples/AppShell.xaml.cs
@@ -1,25 +1,12 @@
-﻿using System.Collections.ObjectModel;
-using Windows.UI.Xaml.Controls;
-using Yugen.Toolkit.Uwp.Samples.Constants;
-using Yugen.Toolkit.Uwp.Samples.Models;
-using Yugen.Toolkit.Uwp.Services;
+﻿using Windows.UI.Xaml.Controls;
 
 namespace Yugen.Toolkit.Uwp.Samples
 {
     public sealed partial class AppShell : Page
     {
-        private readonly ObservableCollection<MenuItem> MenuCollection = new ObservableCollection<MenuItem>(Menu.MenuList);
-
         public AppShell()
         {
             InitializeComponent();
-        }
-
-        private void MenuTree_ItemInvoked(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewItemInvokedEventArgs args)
-        {
-            var tag = args.InvokedItemContainer.Tag?.ToString();
-            if(tag != null)
-                NavigationService.NavigateToPage(tag);
         }
     }
 }
---
+++ Yugen.Toolkit.Uwp.Samples/ViewModels/AppShellViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Toolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Yugen.Toolkit.Standard.Mvvm;
+using Yugen.Toolkit.Uwp.Samples.Constants;
+using Yugen.Toolkit.Uwp.Samples.Models;
+using Yugen.Toolkit.Uwp.Services;
+
+namespace Yugen.Toolkit.Uwp.Samples.ViewModels
+{
+    public class AppShellViewModel : ViewModelBase
+    {
+        public readonly ObservableCollection<MenuItem> MenuCollection = new ObservableCollection<MenuItem>(Menu.MenuList);
+
+        public AppShellViewModel()
+        {
+            NavigationViewOnItemInvokedCommand = new RelayCommand<Microsoft.UI.Xaml.Controls.NavigationViewItemInvokedEventArgs>(NavigationViewOnItemInvokedCommandBehavior);
+        }
+
+        public ICommand NavigationViewOnItemInvokedCommand { get; }
+
+        private void NavigationViewOnItemInvokedCommandBehavior(Microsoft.UI.Xaml.Controls.NavigationViewItemInvokedEventArgs args)
+        {
+            var tag = args.InvokedItemContainer.Tag?.ToString();
+            if (tag != null)
+                NavigationService.NavigateToPage(tag);
+        }
+    }
+}
