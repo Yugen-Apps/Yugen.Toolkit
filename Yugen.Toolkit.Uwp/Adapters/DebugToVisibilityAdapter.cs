1fbc8f71c6db1d721c98b0604b6064b507387920
Emiliano Magliocca <emiliano.magliocca@outlook.com>
2020-09-26 16:11:24 +0400

    Move DebugToVisibilityConverter to static function

---
+++ Yugen.Toolkit.Uwp/Adapters/DebugToVisibilityAdapter.cs
@@ -0,0 +1,29 @@
+﻿using Windows.UI.Xaml;
+
+namespace Yugen.Toolkit.Uwp.Adapters
+{
+    /// <summary>
+    /// Function you can use for x:bind instead of converter
+    /// <code>
+    /// <StackPanel xmlns:converters="using:Yugen.Toolkit.Uwp.Adapters"
+    ///             Visibility="{x:Bind converters:DebugToVisibilityAdapter.Build()}">
+    /// </code>
+    /// </summary>
+    public static class DebugToVisibilityAdapter
+    {
+        /// <summary>
+        /// Converts a DEBUG value to a Visibility value.
+        /// </summary>
+        /// <returns>
+        /// Returns Visibility.Visible if true, else Visibility.Collapsed.
+        /// </returns>
+        public static Visibility Debug()
+        {
+#if DEBUG
+            return Visibility.Visible;
+#else
+            return Visibility.Collapsed;
+#endif
+        }
+    }
+}
