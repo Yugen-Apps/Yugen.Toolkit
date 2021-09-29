e19add5728ece6160f0a95122a8353e91a41761b
Emiliano Magliocca <emiliano.magliocca@outlook.com>
2021-09-29 17:18:30 +0200

    Add readme to Nugets

--- Yugen.Toolkit.Uwp.Samples/Views/HomePage.xaml.cs
+++ Yugen.Toolkit.Uwp.Samples/Views/HomePage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using Windows.UI.Xaml.Controls;
-using Yugen.Toolkit.Uwp.Samples.Helpers;
+﻿using System;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Controls;
 
 namespace Yugen.Toolkit.Uwp.Samples.Views
 {
@@ -11,8 +13,54 @@ namespace Yugen.Toolkit.Uwp.Samples.Views
         public HomePage()
         {
             this.InitializeComponent();
-
-            var summary = DocHelper.ReadSummary("HomePage");
         }
+
+        private async void OnLoadButtonClick(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            FileOpenPicker picker = new FileOpenPicker
+            {
+                SuggestedStartLocation = PickerLocationId.Downloads,
+            };
+            picker.FileTypeFilter.Add("*");
+
+            var file = await picker.PickSingleFileAsync();
+            byte[] bytes;
+            if (file != null)
+            {
+                using (IRandomAccessStream stream = await file.OpenReadAsync())
+                {
+                    using (var reader = new DataReader(stream.GetInputStreamAt(0)))
+                    {
+                        await reader.LoadAsync((uint)stream.Size);
+                        bytes = new byte[stream.Size];
+                        reader.ReadBytes(bytes);
+                    }
+                }
+            }
+            bytes = null;
+        }
+
+        //private async void OnLoadButtonClick(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        //{
+        //    FileOpenPicker picker = new FileOpenPicker
+        //    {
+        //        SuggestedStartLocation = PickerLocationId.Downloads,
+        //    };
+        //    picker.FileTypeFilter.Add("*");
+
+        //    var file = await picker.PickSingleFileAsync();
+        //    if (file != null)
+        //    {
+        //        
+        //        using (Stream stream = await file.OpenStreamForReadAsync())
+        //        {
+        //            using (MemoryStream ms = new MemoryStream())
+        //            {
+        //                stream.CopyTo(ms);
+        //                bytes = ms.ToArray();
+        //            }
+        //        }
+        //    }
+        //}
     }
 }
