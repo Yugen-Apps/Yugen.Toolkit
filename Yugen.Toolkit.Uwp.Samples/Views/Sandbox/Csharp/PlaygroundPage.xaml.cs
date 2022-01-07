9142123c1f7d2e0300cdb26b92165b0e445d3045
Emiliano Magliocca <emiliano.magliocca@outlook.com>
2022-01-07 15:06:04 +0100

    Add Tasks

--- Yugen.Toolkit.Uwp.Samples/Views/Sandbox/Csharp/PlaygroundPage.xaml.cs
+++ Yugen.Toolkit.Uwp.Samples/Views/Sandbox/Csharp/PlaygroundPage.xaml.cs
@@ -78,23 +78,6 @@ namespace Yugen.Toolkit.Uwp.Samples.Views.Sandbox.Csharp
 
             return await picker.PickSingleFileAsync();
         }
-
-        //public async Task RunTasks(WriteableBitmap clone)
-        //{
-        //    var tasks = new List<Task>();
-
-        //    tasks.Add(Task.Run(() => DoWork(400, 1, clone)));
-        //    tasks.Add(Task.Run(() => DoWork(200, 2, clone)));
-        //    tasks.Add(Task.Run(() => DoWork(300, 3, clone)));
-
-        //    await Task.WhenAll(tasks);
-        //}
-
-        //public async Task DoWork(int delay, int n, WriteableBitmap masterImageSource)
-        //{
-        //    await Task.Delay(delay);
-        //    System.Diagnostics.Debug.WriteLine($"{n} {masterImageSource.PixelHeight}");
-        //}
     }
 
     //    public static class SettingsStorageExtensions
--- Yugen.Toolkit.Uwp.Samples/Views/Snippets/Csharp/TasksPage.xaml.cs
+++ Yugen.Toolkit.Uwp.Samples/Views/Snippets/Csharp/TasksPage.xaml.cs
@@ -1,30 +1,105 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Uwp;
+using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Yugen.Toolkit.Uwp.Samples.Views.Snippets.Csharp
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class TasksPage : Page
     {
+        public ObservableCollection<string> MyCollection { get; set; } = new ObservableCollection<string>();
+
+        private readonly DispatcherQueue _dispatcherQueue = DispatcherQueue.GetForCurrentThread();
+        private readonly Random _random = new Random();
+
         public TasksPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void OnStartButtonClick(object sender, RoutedEventArgs e)
+        {
+            MyCollection.Clear();
+            var tasks = new List<Task>();
+
+            for (int i = 0; i < 100; i++)
+            {                
+                tasks.Add(Task.Run(() => DoWork(_random.Next(1, 10), i)));
+            }
+
+            //await Task.WhenAll(tasks);
+        }
+
+        private async void OnStartButton2Click(object sender, RoutedEventArgs e)
+        {
+            MyCollection.Clear();
+            var tasks = new List<Task>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                tasks.Add(DoWork(_random.Next(1, 10), i));
+            }
+
+            //await Task.WhenAll(tasks);
+        }
+
+        private async void OnStartButton3Click(object sender, RoutedEventArgs e)
+        {
+            MyCollection.Clear();
+            var tasks = new List<Task>();
+
+            Parallel.For(0, 100, (i) =>
+            {
+                tasks.Add(Task.Run(() => DoWork(_random.Next(1, 10), i)));
+            });
+
+            //await Task.WhenAll(tasks);
+        }
+
+        private async void OnStartButton4Click(object sender, RoutedEventArgs e)
+        {
+            MyCollection.Clear();
+            var tasks = new List<Task>();
+
+            Parallel.For(0, 100, (i) =>
+            {
+                tasks.Add(DoWork(_random.Next(1, 10), i));
+            });
+
+            //await Task.WhenAll(tasks);
+        }
+
+        private async void OnStartButton5Click(object sender, RoutedEventArgs e)
+        {
+            MyCollection.Clear();
+            var tasks = new List<Func<Task>>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                tasks.Add(() => DoWork(_random.Next(1, 10), i));
+            }
+
+            Parallel.ForEach(tasks, task =>
+            {
+                Task.Run(() => task.Invoke());
+            });
+
+            //await Task.WhenAll(tasks);
+        }
+
+        private async Task DoWork(int delay, int content)
+        {
+            var start = DateTime.Now.Ticks;
+            await Task.Delay(delay);
+            var end = DateTime.Now.Ticks;
+            await _dispatcherQueue.EnqueueAsync(() =>
+            {
+                MyCollection.Add($"{content}: {start}/{end}");
+            });
         }
     }
 }
